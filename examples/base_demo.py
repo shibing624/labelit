205f9ef9b4f87155ee1bf55f2476aae3539adc99
xuming624 <xuming624@qq.com>
2021-01-05 22:30:29 +0800

    update demo

--- examples/base_demo.py
+++ examples/base_demo.py
@@ -13,7 +13,7 @@ from labelit.label import LabelModel
 pwd_path = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == '__main__':
-    file_path = os.path.join(pwd_path, "../extra_data/bjp.txt")
+    file_path = os.path.join(pwd_path, "../extra_data/samples.txt")
 
     lm = LabelModel(input_file_path=file_path)
     lm.label()
